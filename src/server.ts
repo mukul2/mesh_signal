fb890af31514841cb2ae66a4573f8cfaed5fedbf
MD AMINUL ISLAM MUKUL <saidur.shawon@gmail.com>
2021-07-04 01:33:37 +0100

    Update server.ts

--- src/server.ts
+++ src/server.ts
@@ -24,7 +24,7 @@ const options: cors.CorsOptions = {
   ],
   credentials: true,
   methods: 'GET,HEAD,OPTIONS,PUT,PATCH,POST,DELETE',
-  origin: API_URL,
+  origin:'*',
   preflightContinue: false,
 };
 
